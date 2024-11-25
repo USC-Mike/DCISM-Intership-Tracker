0405f5a46609e06dbb98b5c577c4be7feb3f5046
Mike Pino <kurtmikepino@192.168.1.2>
2024-11-26 02:14:13 +0800

    Initially setup the web app.

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+      "./*.php",
+      "./src/**/*.{php,html,js}",
+      "./public/**/*.{php,html,js}",
+  ],
+  theme: {
+      extend: {},
+  },
+  plugins: [],
+};
